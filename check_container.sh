32be11f673ed4a08a84936ef0b0271aca8b75209
CHICHIK <andreyshigin@icloud.com>
2026-07-13 22:06:30 +0300

    Update script, visual part

--- check_container.sh
+++ check_container.sh
@@ -16,28 +16,28 @@ fi
 checkCON=$(docker ps -a | grep "$1")
 if [ -n "$checkCON" ]
 then
-	echo "$(date) CONTAINER EXISTS"
+	echo "[$(date)] CONTAINER EXISTS"
 		checkCONRUN=$(docker ps | grep "$1")
 			if [ -n "$checkCONRUN" ]
 			then
-				echo "$(date) CONTAINER RUNNING"
+				echo "[$(date)] CONTAINER RUNNING"
 					checkCONPORT=$(ss -ltnp | grep ":$2")
 					if [ -n "$checkCONPORT" ]
 					then
-						echo "$(date) PORT OK"
+						echo "[$(date)] PORT OK"
 							httpCHECK=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:$2")
 							if [ "$httpCHECK" != "000" ]
 							then
-								echo "$(date) HTTP RESPONSE: $httpCHECK"
+								echo "[$(date)] HTTP RESPONSE: $httpCHECK"
 							else
-								echo "$(date) HTTP FAIL"
+								echo "[$(date)] HTTP FAIL"
 							fi
 					else
-						echo "$(date) PORT FAIL"
+						echo "[$(date)] PORT FAIL"
 					fi
 			else
-				echo "$(date) CONTAINER NOT RUNNING"
+				echo "[$(date)] CONTAINER NOT RUNNING"
 			fi
 else
-	echo "$(date) NOT FOUND CONTAINER"
+	echo "[$(date)] NOT FOUND CONTAINER"
 fi
